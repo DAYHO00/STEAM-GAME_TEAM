16a0f6292204abadd5106b96341eeedd130785dd
cksals314 <cksals314@gmail.com>
2025-11-19 22:05:28 +0900

    init 수정

--- backend/recommend/__init__.py
+++ backend/recommend/__init__.py
@@ -1,2 +1,4 @@
-from recommend.user_based import recommend_by_user
-from recommend.item_based import recommend_by_item
+from .user_based import recommend_by_user
+from .item_based import recommend_by_item
+
+__all__ = ["recommend_by_user", "recommend_by_item"] 
